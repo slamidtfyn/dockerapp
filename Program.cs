0e10ab62e27224c9678e29eb387e99e13bd2f4a8
Søren Larsen <sl@komit.nu>
2019-02-21 00:23:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dockerapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(new { Result = "Ok" }));
+        }
+    }
+}
